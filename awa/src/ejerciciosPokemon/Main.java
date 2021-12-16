03d07e657d98dee2a13b34b117a22131d129cff0
Jorge <Jorge@DESKTOP-44FKTKN>
2021-12-16 19:02:24 +0100

    pokemons

--- awa/src/ejerciciosPokemon/Main.java
+++ awa/src/ejerciciosPokemon/Main.java
@@ -1,33 +1,183 @@
 package ejerciciosPokemon;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		Pokemon charmander = new Pokemon("Charmander", "Fuego", 500);
 		
-		Ataque atk1 = new Ataque("Ascuas", "Fuego", 100);
-		Ataque atk2 = new Ataque("Placaje", "Normal", 60);
-		Ataque atk3 = new Ataque("Llamarada", "Fuego", 150);
+		Scanner scan = new Scanner(System.in);
 		
+		//ATAQUES
+		Ataque ascuas = new Ataque("Ascuas", "Fuego", 60);
+		Ataque placaje = new Ataque("Placaje", "Normal", 40);
+		Ataque coligneo = new Ataque("Colmillo Igneo", "Fuego", 70);
+		Ataque cascada = new Ataque("Cascada", "Agua", 70);
+		Ataque latcepa = new Ataque("Latigo Cepa", "Planta", 60);
+		Ataque hoafi = new Ataque("Hoja Afilada", "Normal", 70);
+		Ataque derribo = new Ataque("Derribo", "Normal", 70);
+		Ataque burbuja = new Ataque("Burbuja", "Agua", 40);
+		Ataque mordisco = new Ataque("Mordisco", "Siniestro", 60);
+		Ataque impactrueno = new Ataque("Impactrueno", "Electrico", 40);
+		Ataque rayo = new Ataque("Rayo", "Electrico", 70);
+		Ataque picadura = new Ataque("Picadura", "Bicho", 40);
+		Ataque tornado = new Ataque("Tornado", "Volador", 70);
+		Ataque confusion = new Ataque("Confusion", "Psiquico", 55);
+		Ataque picvenenoso = new Ataque("Picotazo Venenoso", "Veneno", 50);
+		Ataque golpeka = new Ataque("Golpe Karate", "Lucha", 60);
+		
+		
+		//POKEMONS
+		Pokemon charmander = new Pokemon("Charmander", "Fuego", 350);
 		charmander.ataques = new ArrayList<Ataque>();
-		charmander.ataques.add(atk1);
-		charmander.ataques.add(atk2);
-		charmander.ataques.add(atk3);
-		
-		Pokemon bulbasaur = new Pokemon("Bulbasaur", "Planta", 500);
-		
-		Ataque atk4 = new Ataque("Placaje", "Normal", 60);
-		Ataque atk5 = new Ataque("Latigo Cepa", "Planta", 100);
-		Ataque atk6 = new Ataque("Hoja Afilada", "Normal", 150);
-		
+		charmander.ataques.add(ascuas);
+		charmander.ataques.add(placaje);
+		charmander.ataques.add(coligneo);
+		charmander.ataques.add(cascada);
+
+		Pokemon bulbasaur = new Pokemon("Bulbasaur", "Planta", 350);
 		bulbasaur.ataques = new ArrayList<Ataque>();
-		bulbasaur.ataques.add(atk4);
-		bulbasaur.ataques.add(atk5);
-		bulbasaur.ataques.add(atk6);
+		bulbasaur.ataques.add(placaje);
+		bulbasaur.ataques.add(latcepa);
+		bulbasaur.ataques.add(hoafi);
+		bulbasaur.ataques.add(derribo);
 		
+		Pokemon squirtle = new Pokemon("Squirtle", "Agua", 350);
+		squirtle.ataques = new ArrayList<Ataque>();
+		squirtle.ataques.add(placaje);
+		squirtle.ataques.add(cascada);
+		squirtle.ataques.add(burbuja);
+		squirtle.ataques.add(mordisco);
+		
+		Pokemon pikachu = new Pokemon("Pikachu", "Electrico", 300);
+		pikachu.ataques = new ArrayList<Ataque>();
+		pikachu.ataques.add(placaje);
+		pikachu.ataques.add(impactrueno);
+		pikachu.ataques.add(rayo);
+		pikachu.ataques.add(derribo);
+		
+		Pokemon caterpie = new Pokemon("Caterpie", "Bicho", 200);
+		caterpie.ataques = new ArrayList<Ataque>();
+		caterpie.ataques.add(placaje);
+		caterpie.ataques.add(picadura);
+		caterpie.ataques.add(tornado);
+		caterpie.ataques.add(confusion);
+		
+		Pokemon ekans = new Pokemon("Ekans", "Veneno", 250);
+		ekans.ataques = new ArrayList<Ataque>();
+		ekans.ataques.add(placaje);
+		ekans.ataques.add(picadura);
+		ekans.ataques.add(mordisco);
+		ekans.ataques.add(picvenenoso);
+		
+		Pokemon pidgey = new Pokemon("Pidgey", "Volador", 250);
+		pidgey.ataques = new ArrayList<Ataque>();
+		pidgey.ataques.add(placaje);
+		pidgey.ataques.add(tornado);
+		pidgey.ataques.add(derribo);
+		pidgey.ataques.add(picvenenoso);
+		
+		Pokemon mankey = new Pokemon("Mankey", "Lucha", 300);
+		mankey.ataques = new ArrayList<Ataque>();
+		mankey.ataques.add(placaje);
+		mankey.ataques.add(mordisco);
+		mankey.ataques.add(derribo);
+		mankey.ataques.add(golpeka);
+		
+
+		//COMBATE
+		
+		Pokemon pokeElegido = new Pokemon();
+		Pokemon pokeOponente = new Pokemon();
+		
+		System.out.println("-----BIENVENIDO AL COMBATE POKEMON-----\n");
+		System.out.println("Que Pokemon quieres utilizar?\n\n(1) Charmander\n(2) Bulbasaur\n(3) Squirtle\n(4) Pikachu\n(5) Caterpie\n(6) Ekans\n(7) Pidgey\n(8) Mankey\n\nElije aqui: ");
+		int eleccion = scan.nextInt();
+		System.out.println("\nContra que Pokemon quieres combatir?\n\n(1) Charmander\n(2) Bulbasaur\n(3) Squirtle\n(4) Pikachu\n(5) Caterpie\n(6) Ekans\n(7) Pidgey\n(8) Mankey\n\nElije aqui: ");
+		int oponente = scan.nextInt();
+		
+		
+		switch(eleccion) {
+		case 1:
+			pokeElegido = charmander;
+			break;
+		case 2:
+			pokeElegido = bulbasaur;
+			break;
+		case 3:
+			pokeElegido = squirtle;
+			break;
+		case 4:
+			pokeElegido = pikachu;
+			break;
+		case 5:
+			pokeElegido = caterpie;
+			break;
+		case 6:
+			pokeElegido = ekans;
+			break;
+		case 7:
+			pokeElegido = pidgey;
+			break;
+		case 8:
+			pokeElegido = mankey;
+			break;
+		}
+		
+		switch(oponente) {
+		case 1:
+			pokeOponente = charmander;
+			break;
+		case 2:
+			pokeOponente = bulbasaur;
+			break;
+		case 3:
+			pokeOponente = squirtle;
+			break;
+		case 4:
+			pokeOponente = pikachu;
+			break;
+		case 5:
+			pokeOponente = caterpie;
+			break;
+		case 6:
+			pokeOponente = ekans;
+			break;
+		case 7:
+			pokeOponente = pidgey;
+			break;
+		case 8:
+			pokeOponente = mankey;
+			break;
+		}
+		
+		System.out.println("\n------------------------------------------------------------\n\nUn "+pokeOponente.nombre+" salvaje ha aparecido entre la hierba!");
+		scan.nextLine();
+		
+		int i = 1;
+		while(pokeElegido.vida > 0 && pokeOponente.vida > 0) {
+			if(i%2 != 0) {
+				System.out.println("Elige un ataque (0) "+pokeElegido.ataques.get(0).nombre+" (1) "+pokeElegido.ataques.get(1).nombre+" (2) "+pokeElegido.ataques.get(2).nombre+" (3) "+pokeElegido.ataques.get(3).nombre+" : ");
+				int atkEle = scan.nextInt();
+				pokeOponente.checkAtk(pokeElegido, pokeElegido.ataques.get(atkEle));
+			} else {
+				int random = (int)(Math.random()*3+0); 
+				pokeElegido.checkAtk(pokeOponente, pokeOponente.ataques.get(random));
+			}
+			
+			if(pokeElegido.vida < 0) {
+				System.out.println(pokeElegido.nombre+" se ha ido con diosito.");
+			} 
+			
+			if(pokeOponente.vida < 0) {
+				System.out.println(pokeOponente.nombre+" se ha ido con diosito.");
+			}
+		i++;
+		}
+	
+		
+		}
 		
 	}
-}
+
--- awa/src/ejerciciosPokemon/Pokemon.java
+++ awa/src/ejerciciosPokemon/Pokemon.java
@@ -1,15 +1,22 @@
 package ejerciciosPokemon;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Pokemon {
+	
+	Scanner scan = new Scanner(System.in);
 
+	//ATRIBUTOS
+	
 	String nombre;
 	String lore;
 	String tipo;
 	int fuerza;
 	int vida;
 	ArrayList<Ataque> ataques;
+	
+	//CONSTRUCTOR
 	
 	public Pokemon(String nombre, String tipo, int vida) {
 		
@@ -18,9 +25,160 @@ public class Pokemon {
 		this.vida = vida;
 	}
 	
-	public void RecibeAtk() {
-		
+	public Pokemon() {
 		
 	}
 	
+	//METODOS recibeAtk
+	
+	public void recibeAtk(Pokemon poke, Ataque atk){
+		this.vida -= atk.cantdanio;
+		System.out.println(poke.nombre+" ha usado "+atk.nombre+".");
+		scan.nextLine();
+		System.out.println(this.nombre+" ha perdido "+atk.cantdanio+" de HP.");
+		scan.nextLine();
+		System.out.println(this.nombre+" ahora tiene "+this.vida+" HP.");
+	}
+	
+	public void recibeAtkEficaz(Pokemon poke, Ataque atk) {
+		this.vida -= (atk.cantdanio * 1.5);
+		System.out.println(poke.nombre+" ha usado "+atk.nombre+".");
+		scan.nextLine();
+		System.out.println("Ha sido muy efectivo!");
+		scan.nextLine();
+		System.out.println(this.nombre+" ha perdido "+(atk.cantdanio*1.5)+" de HP.");
+		scan.nextLine();
+		System.out.println(this.nombre+" ahora tiene "+this.vida+" HP.");
+	}
+	
+	public void recibeAtkPoco(Pokemon poke, Ataque atk) {
+		this.vida -= (atk.cantdanio * 0.5);
+		System.out.println(poke.nombre+" ha usado "+atk.nombre+".");
+		scan.nextLine();
+		System.out.println("No ha sido muy eficaz...");
+		scan.nextLine();
+		System.out.println(this.nombre+" ha perdido "+(atk.cantdanio*0.5)+" de HP.");
+		scan.nextLine();
+		System.out.println(this.nombre+" ahora tiene "+this.vida+" HP.");
+		
+	}
+	
+	//METODOS EFECTIVIDAD
+	
+	public void planta (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Fuego" | atk.tipo == "Hielo" | atk.tipo == "Veneno" | atk.tipo == "Volador" | atk.tipo == "Bicho") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Agua" | atk.tipo == "Electrico" | atk.tipo == "Planta" | atk.tipo == "Tierra") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void fuego (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Agua" | atk.tipo == "Roca" | atk.tipo == "Tierra") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Acero" | atk.tipo == "Bicho" | atk.tipo == "Roca" | atk.tipo == "Tierra" | atk.tipo == "Fuego" | atk.tipo == "Hada" | atk.tipo == "Hielo" | atk.tipo == "Planta") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void bicho (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Fuego" | atk.tipo == "Roca" | atk.tipo == "Volador") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Lucha" | atk.tipo == "Planta" | atk.tipo == "Tierra") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void volador (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Electrico" | atk.tipo == "Hielo" | atk.tipo == "Roca") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Bicho" | atk.tipo == "Lucha" | atk.tipo == "Planta") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void agua (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Electrico" | atk.tipo == "Planta") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Agua" | atk.tipo == "Acero" | atk.tipo == "Fuego" | atk.tipo == "Hielo") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void electrico (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Tierra") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Volador" | atk.tipo == "Electrico" | atk.tipo == "Acero") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void veneno (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Psiquico" | atk.tipo == "Tierra") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Bicho" | atk.tipo == "Hada" | atk.tipo == "Lucha" | atk.tipo == "Planta" | atk.tipo == "Veneno") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void normal (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Lucha") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	public void lucha (Pokemon poke, Ataque atk) {
+		if(atk.tipo == "Hada" | atk.tipo == "Psiquico" | atk.tipo == "Volador") {	
+			this.recibeAtkEficaz(poke, atk);
+		} else if (atk.tipo == "Bicho" | atk.tipo == "Roca" | atk.tipo == "Siniestro") {
+			this.recibeAtkPoco(poke, atk);
+		} else {
+			this.recibeAtk(poke, atk);
+		}
+	}
+	
+	
+	
+	//METODO CHECK ATAQUE
+	
+	public void checkAtk (Pokemon poke, Ataque atk) {
+		if (this.tipo == "Planta") {
+			this.planta(poke, atk);
+		} else if (this.tipo == "Fuego") {
+			this.fuego(poke, atk);
+		} else if (this.tipo == "Bicho") {
+			this.bicho(poke, atk);
+		} else if (this.tipo == "Volador") {
+			this.volador(poke, atk);
+		} else if (this.tipo == "Agua") {
+			this.agua(poke, atk);
+		} else if (this.tipo == "Electrico") {
+			this.electrico(poke, atk);
+		} else if (this.tipo == "Veneno") {
+			this.veneno(poke, atk);
+		} else if (this.tipo == "Normal") {
+			this.normal(poke, atk);
+		} else if (this.tipo == "Lucha") {
+			this.lucha(poke, atk);
+		}
+	}
+	
+		
 }
+	
